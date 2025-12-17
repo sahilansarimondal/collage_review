e335de0e1c16ebdebf2ea3f91c93e97bc9801e4e
sahil ansari tp <sahilansarimondal@gmail.com>
2025-12-17 11:53:06 +0530

    added the next.js in web and basic navbar and here section

--- apps/api/src/config/prisma.ts
+++ apps/api/src/config/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
