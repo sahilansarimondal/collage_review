7e2fe5d9e11e45eb1a2a8df615559f2ab5442d99
sahil ansari tp <sahilansarimondal@gmail.com>
2025-12-17 11:53:06 +0530

    added the next.js in web and basic navbar and here section

---
+++ apps/web/src/app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSectionOne from "@/components/hero-section-demo-1";
+import { NavbarDemo } from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* <NavbarDemo /> */}
+      <HeroSectionOne />
+    </div>
+  );
+}
